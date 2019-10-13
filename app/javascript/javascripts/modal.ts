4d2e44d042d24074aeed35b73ef682303b1c1c7f
s.tomoki <tanajammw@gmail.com>
2019-10-14 03:10:10 +0900

    Fixed modal events

--- app/javascript/javascripts/modal.ts
+++ app/javascript/javascripts/modal.ts
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   let elems = document.querySelectorAll('.modal');
   M.Modal.init(elems, {});
+
+  document.body.addEventListener('ajax:success', function(event) {
+    let elems = document.querySelectorAll('.modal');
+    M.Modal.init(elems, {});
+  })
 });
