da5b6f39f7a9277202d961f74f0006a75e5e0f55
s.tomoki <tanajammw@gmail.com>
2019-10-15 00:17:30 +0900

    Fixed modal events for pagination

--- app/javascript/javascripts/modal.ts
+++ app/javascript/javascripts/modal.ts
@@ -2,8 +2,33 @@ document.addEventListener('DOMContentLoaded', function() {
   let elems = document.querySelectorAll('.modal');
   M.Modal.init(elems, {});
 
+  // 初期化
   document.body.addEventListener('ajax:success', function(event) {
     let elems = document.querySelectorAll('.modal');
     M.Modal.init(elems, {});
-  })
+  });
+
+  // 初期化
+  document.getElementById('paginator').addEventListener('ajax:success', function(event) {
+    let elems = document.querySelectorAll('.modal');
+    M.Modal.init(elems, {});
+  });
+
+  const el = document.getElementById('search_result')
+  const observer = new MutationObserver(function(){
+    /** DOMの変化が起こった時の処理 */
+    document.getElementById('paginator').addEventListener('ajax:success', function(event) {
+      let elems = document.querySelectorAll('.modal');
+      M.Modal.init(elems, {});
+    });
+  });
+
+  const config = { 
+    attributes: true, 
+    childList: true, 
+    characterData: true 
+  };
+  
+  /** 要素の変化監視をスタート */
+  observer.observe(el, config);
 });
